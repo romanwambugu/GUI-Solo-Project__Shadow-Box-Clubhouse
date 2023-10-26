77fe9b44d0f55cf3b4a52a581f9125f39e8a82b1
romanwambugu <rwambugu172@lsr7.net>
2023-10-26 12:30:16 -0500

    Day 2 - Debugging

--- main.py
+++ main.py
@@ -13,37 +13,37 @@ class ShadowBox(QWidget):
         self.setLayout(main_layout)
 
         # Add Images to Layout
-        MickeyGlove_RightImage = QPixmap("Image Folder/MickeyGlove_Right.png")
-        MickeyGlove_RightLabel = QLabel(self)
-        MickeyGlove_RightLabel.setPixmap(MickeyGlove_RightImage)
-        main_layout.addWidget(MickeyGlove_RightLabel, 3, 0)
+        Glove_RightImage = QPixmap("Image Folder/MickeyGlove_Right.png")
+        Glove_RightLabel = QLabel(self)
+        Glove_RightLabel.setPixmap(Glove_RightImage)
+        main_layout.addWidget(Glove_RightLabel, 3, 0)
 
 
-        MickeyGlove_UpImage = QPixmap("Image Folder/MickeyGlove_Up.png")
-        MickeyGlove_UpLabel = QLabel(self)
-        MickeyGlove_UpLabel.setPixmap(MickeyGlove_RightImage)
-        main_layout.addWidget(MickeyGlove_UpImage, 3, 1)
+        Glove_UpImage = QPixmap("Image Folder/MickeyGlove_Up.png")
+        Glove_UpLabel = QLabel(self)
+        Glove_UpLabel.setPixmap(Glove_UpImage)
+        main_layout.addWidget(Glove_UpLabel, 3, 1)
 
 
-        MickeyGlove_DownImage = QPixmap("Image Folder/MickeyGlove_Down.png")
-        MickeyGlove_DownLabel = QLabel(self)
-        MickeyGlove_DownLabel.setPixmap(MickeyGlove_RightImage)
-        main_layout.addWidget(MickeyGlove_DownImage, 3, 2)
+        Glove_DownImage = QPixmap("Image Folder/MickeyGlove_Down.png")
+        Glove_DownLabel = QLabel(self)
+        Glove_DownLabel.setPixmap(Glove_DownImage)
+        main_layout.addWidget(Glove_DownLabel, 3, 2)
 
 
-        MickeyGlove_LeftImage = QPixmap("Image Folder/MickeyGlove_Left.png")
-        MickeyGlove_LeftLabel = QLabel(self)
-        MickeyGlove_LeftLabel.setPixmap(MickeyGlove_RightImage)
-        main_layout.addWidget(MickeyGlove_LeftImage, 3, 3)
+        Glove_LeftImage = QPixmap("Image Folder/MickeyGlove_Left.png")
+        Glove_LeftLabel = QLabel(self)
+        Glove_LeftLabel.setPixmap(Glove_LeftImage)
+        main_layout.addWidget(Glove_LeftLabel, 3, 3)
 
 
-        # Direction Buttons
+        #Direction Buttons
         Up_button = QPushButton("Up")
         Down_button = QPushButton("Down")
         Left_button = QPushButton("Left")
         Right_button = QPushButton("Right")
 
-        # Add Buttons to Layout
+        #Add Buttons to Layout
         main_layout.addWidget(Up_button, 0, 1)
         main_layout.addWidget(Left_button, 1, 0)
         main_layout.addWidget(Right_button, 1, 2)
